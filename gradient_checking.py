c85bbbb5258d49d18f467485defc69ef7cb2c82b
Matthew Weidman <weidman.matthew@gmail.com>
2016-08-31 11:49:38 -0600

    workig on check_head()

--- gradient_checking.py
+++ gradient_checking.py
@@ -61,13 +61,13 @@ def check_numerical_gradient_scalar():
 
 def check_w_tilde():
     n = 10
-    w = softmax(np.random.randn(n))
+    wt = softmax(np.random.randn(n))
     gamma = np.random.randn()
-    f = lambda: w**gamma / (w**gamma).sum()
-    n_grad = numerical_gradient_array(f, w)
-    wg = w**gamma
-    wg1 = w**(gamma-1)
-    grad = gamma * wg1 / wg.sum() * (1-wg/wg.sum())
+    f = lambda: wt**gamma / (wt**gamma).sum()
+    n_grad = numerical_gradient_array(f, wt)
+    wgamma = wt**gamma
+    wgsum = wgamma.sum()
+    grad = gamma * (wt**(gamma-1))/wgsum * (1-wgamma/wgsum)
     diff = n_grad - grad
     print(diff)
 
@@ -86,6 +86,7 @@ def circular_convolve(a, b):
 
 def check_convolve():
     n = 9
+    x = 1/(1+np.exp(-np.random.randn(n)))
     wt = softmax(np.random.randn(n))
     s = softmax(np.random.randn(n))
     f = lambda: circular_convolve(wt, s)
@@ -139,8 +140,7 @@ def check_key():
     beta = np.random.randn()
     k = np.random.randn(1, m)
     M = np.random.randn(n, m)
-    f = lambda: beta * k.dot(M.T) / (np.linalg.norm(k) *
-        np.linalg.norm(M, axis=1))
+    f = lambda: beta*k.dot(M.T) / (np.linalg.norm(k)*np.linalg.norm(M, axis=1))
 
     # gradient with respect to k
     n_grad = numerical_gradient_matrix(f, k)
@@ -152,8 +152,65 @@ def check_key():
 
     # gradient with respect to M
     n_grad = numerical_gradient_matrix(f, M)
-    grad = beta / (k_norm * M_norm) * (k - M * k.dot(M.T).T / M_norm**2)
+    grad = beta / (k_norm * M_norm) * (k - M * M.dot(k.T) / M_norm**2)
     print(n_grad-grad)
 
+def compute_w(w_prev, M_prev, k, beta, g, s, gamma):
+    u = beta * k.dot(M_prev.T)/(np.linalg.norm(k)*np.linalg.norm(M_prev, axis=1))
+    wc = softmax(u)
+    wg = g*wc + (1-g)*w_prev
+    wt = circular_convolve(wg, s)
+    w = wt**gamma / (wt**gamma).sum()
+    return u, wc, wg, wt, w
+
+def backprop_w(w_prev, M_prev, k, beta, g, s, gamma, u, wc, wg, wt, grad_in):
+    wgamma = wt**gamma
+    wgsum = wgamma.sum()
+    dwt = grad_in * gamma * (wt**(gamma-1))/wgsum * (1-wgamma/wgsum)
+    dgamma = grad_in * wgamma/wgsum**2 * (np.log(wt)*wgsum -
+        np.sum(wgamma*np.log(wt)))
+    dwg = dwt * s[0] * np.ones(wg.shape)
+    ds = dwt * wg[0] * np.ones(s.shape)
+    dwc = dwg * g * np.ones(wc.shape)
+    dw_prev = dwg * (1-g) * np.ones(w_prev.shape)
+    dg = dwg * (wc - w_prev)
+    du = dwc * u * (1-u)
+    dbeta = du * u / beta
+    k = k[np.newaxis,:]
+    k_norm = np.sqrt((k**2).sum())
+    M_norm = np.sqrt((M_prev**2).sum(axis=1))[:,np.newaxis]
+    dk = du[:,np.newaxis] * beta / (k_norm * M_norm) * (M_prev -
+        M_prev.dot(k.T).dot(k) / k_norm**2)
+    dM_prev = du[:,np.newaxis] * beta / (k_norm * M_norm) * (k -
+        M_prev * M_prev.dot(k.T) / M_norm**2)
+    return dwt, dgamma, dwg, ds, dwc, dw_prev, dg, du, dbeta, dk, dM_prev
+
+def forward_back_w(w_prev, M_prev, k, beta, g, s, gamma, grad_in):
+    u, wc, wg, wt, w = compute_w(w_prev, M_prev, k, beta, g, s, gamma)
+    return backprop_w(w_prev, M_prev, k, beta, g, s, gamma, u, wc, wg, wt,
+        grad_in)
+
+def check_head():
+    n = 9
+    m = 11
+    w_prev = softmax(np.random.randn(n))
+    M_prev = np.random.randn(n, m)
+    k = np.random.randn(m)
+    beta = np.random.randn()
+    g = 1/(1+np.exp(-np.random.randn()))
+    s = softmax(np.random.randn(n))
+    gamma = np.random.randn()
+    f = lambda: compute_w(w_prev, M_prev, k, beta, g, s, gamma)[-1]
+    grad = forward_back_w(w_prev, M_prev, k, beta, g, s, gamma, 1)
+
+    # check dgamma
+    f_gamma = lambda gamma: compute_w(w_prev, M_prev, k, beta, g, s, gamma)[-1]
+    n_dgamma = numerical_gradient_scalar(f_gamma, gamma)
+    print(grad[1] - n_dgamma)
+
+    # check ds
+    n_ds = numerical_gradient_array(f, s)
+    print(grad[3] - n_ds)
+
 if __name__ == "__main__":
-    check_key()
+    check_head()
